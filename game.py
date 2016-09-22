056cb6f37a5690783ee65a5626b5b2335184867b
DIWithers <d.ivy.withers@gmail.com>
2016-09-22 11:34:08 -0400

    Use ball as placeholder for hero

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 import sys # we will need the user to be able to quit
 import pygame #imports package with all available pygame modules
 from hero import Hero #bring in the hero class with all it's methods and glory
+from settings import Settings
 
 #Set up the main core function
 def run_game():
 	pygame.init() #initializes pygame modules
+	game_settings = Settings()
 	screen_size = (1000, 800)
 	# screen = pygame.display.set_mode((1000, 800)) #set screen size, need double parenthesis, or set width and height to variable
-	screen = pygame.display.set_mode(screen_size)
+	screen = pygame.display.set_mode(game_settings.screen_size)
 	pygame.display.set_caption("Monster Attack") #set msg on status bar
 
 	bg_color = (82, 111, 53) #green grass color
@@ -19,7 +21,7 @@ def run_game():
 			if event.type == pygame.QUIT:
 				sys.exit()
 		#fill the background(bg) with our green
-		screen.fill(bg_color)
+		screen.fill(game_settings.bg_color)
 		hero.draw_me() #call the draw method and put the hero on the screen
 		pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,5 @@
+#A place for settings...
+class Setting():
+	def __init__(self):
+		self.screen_size = (1000, 800)
+		self.bg_color = (82, 111, 53) #green grass color
